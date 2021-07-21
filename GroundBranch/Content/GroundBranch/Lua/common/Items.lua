1170ce377c7935cceabb299ca474b69a3eef9ca3
Jakub Baranowski <jbaranowski@gog.com>
2021-07-21 06:32:40 +0200

    Started work on Items script It should help with restricing player equipment in game modes.

---
+++ GroundBranch/Content/GroundBranch/Lua/common/Items.lua
@@ -0,0 +1,55 @@
+local Items = {
+    Primaries = {
+        SMG = {
+            "AKS74U",
+            "MP5A4",
+            "MP5A5",
+            "MP5SD5",
+            "MP5SD6",
+            "MP7A1",
+            "MPX_SBR",
+            "UMP45"
+        },
+        AR = {
+            "AK74_MI",
+            "AK74_MI_CQB",
+            "AK74M",
+            "AKM",
+            "FAL_Vintage",
+            "FAL_Tactical",
+            "Galil_SAR",
+            "M16A4",
+            "M416D",
+            "M416D_CQB",
+            "M4A1",
+            "M4A1_Block_II",
+            "MK18_MOD_1",
+        }
+    },
+    Generic = {
+        GrenadePouch = "Pouch_Grenade",
+        AmmoPouch = "Pouch_Ammo"
+    }
+}
+
+function Items.CheckIfHasForbiddenItems(playerInventory)
+    for index, value in ipairs(playerInventory) do
+        print(index)
+        print(value)
+        print("Table")
+        for k, v in pairs(value) do
+            print(k)
+            print(v)
+        end
+        print("Metatable")
+        local metatable = getmetatable(value)
+        for k, v in pairs(metatable) do
+            print(k)
+            print(v)
+        end
+    end
+end
+
+Items.__index = Items
+
+return Items
