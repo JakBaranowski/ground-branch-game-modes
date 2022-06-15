2af8d1c2c20a41867d90cd56a1d5b05c3bb44b47
Bob/AT <BobAT@gmx.at>
2022-06-16 00:26:00 +0200

    Add Tables.Copy() and Tables.DeepCopy() methods
    Table shallow copy and deep copy code adapted from
    Penlight's tablex <https://github.com/lunarmodules/Penlight>

--- GroundBranch/Content/GroundBranch/Lua/common/Tables.lua
+++ GroundBranch/Content/GroundBranch/Lua/common/Tables.lua
@@ -88,4 +88,88 @@ function Tables.Index(table, value)
     return nil
 end
 
+--[[
+Table shallow copy and deep copy code adapted from Penlight's tablex <https://github.com/lunarmodules/Penlight>
+
+Copyright (C) 2009-2016 Steve Donovan, David Manura.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT
+SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+]]--
+
+local function complain (idx,msg)
+    print("ERROR! Argument " .. idx .. " is not " + msg)
+end
+
+local function check_meta (val)
+    if type(val) == 'table' then return true end
+    return getmetatable(val)
+end
+
+local function types_is_iterable (val)
+    local mt = check_meta(val)
+    if mt == true then return true end
+    return mt and mt.__pairs and true
+end
+
+local function assert_arg_iterable (idx,val)
+    if not types_is_iterable(val) then
+        complain(idx,"iterable")
+    end
+end
+
+--- make a shallow copy of a table
+--- @param tab table an iterable source
+--- @return table new table
+function Tables.Copy (t)
+    assert_arg_iterable(1,t)
+    local res = {}
+    for k,v in pairs(t) do
+        res[k] = v
+    end
+    return res
+end
+
+local function cycle_aware_copy(t, cache)
+    if type(t) ~= 'table' then return t end
+    if cache[t] then return cache[t] end
+    assert_arg_iterable(1,t)
+    local res = {}
+    cache[t] = res
+    local mt = getmetatable(t)
+    for k,v in pairs(t) do
+        k = cycle_aware_copy(k, cache)
+        v = cycle_aware_copy(v, cache)
+        res[k] = v
+    end
+    setmetatable(res,mt)
+    return res
+end
+
+--- make a deep copy of a table, recursively copying all the keys and fields.
+--- This supports cycles in tables; cycles will be reproduced in the copy.
+--- This will also set the copied table's metatable to that of the original.
+--- @param tab table A table
+--- @return table new table
+function Tables.DeepCopy(t)
+    return cycle_aware_copy(t,{})
+end
+
 return Tables
